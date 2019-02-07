ba67b4d49a1ffb9200e16b185a078fabf13521a5
linghs <a4643895@163.com>
2019-02-07 14:43:32 +0800

    加入前端文件

---
+++ view/precache-manifest.dc4fc2caa0cce2fa80233505044c9ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ea1464be819ad992eb",
+    "url": "./static/js/main.40ea1464.chunk.js"
+  },
+  {
+    "revision": "b2ca720b2189e59a3812",
+    "url": "./static/css/1.3897758d.chunk.css"
+  },
+  {
+    "revision": "b2ca720b2189e59a3812",
+    "url": "./static/js/1.b2ca720b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5dcb856423b3c62467497b0e73cbf20a",
+    "url": "./index.html"
+  }
+];
