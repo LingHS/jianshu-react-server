7ce325b7c7fe4a4b27a5c46974f270d2a3a8532e
linghs <a4643895@163.com>
2019-02-08 18:22:51 +0800

    CI

---
+++ .circleci/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+SSH_USER=root
+SSH_HOST=47.94.96.223
+SRC=`pwd`"/"
+DES=/home/web
+NOW=`date +"%Y-%m-%d %H:%M:%S"`
+
+
+rsync -vzrc --delete --exclude ".git" --exclude ".env" --exclude ".circleci" --exclude "src" --exclude "tsconfig.json" $SRC $SSH_USER@$SSH_HOST:$DES
+ssh $SSH_USER@$SSH_HOST "<remote deploy command>"
